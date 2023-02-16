f8f0d16063d2287584a1c6e6a7207426b3e90453
ArpitxGit <arpitxdungeon@gmail.com>
2023-02-16 13:49:15 +0530

    cleared repo

--- near8testing-react-app/src/actions/types.js
+++
@@ -1,3 +0,0 @@
-export const ALL_USERS = "ALL_USERS";
-export const SET_METAMASK_ADDRESS = "SET_METAMASK_ADDRESS";
-export const NO_ACTION = "NO_ACTION";
--- near8testing-react-app/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import metamask from "./metamask";
-
-export default combineReducers({
-    metamask,
-});
